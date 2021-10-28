2cdb424adf99e9b235cefe2f87c78f8adbf04ef8
Subrat Behera <denime.devil@gmail.com>
2021-10-28 18:42:50 +0530

    Dynamic form for Input Text & textarea box

--- src/app/components/new-sandbox/new-sandbox.component.ts
+++ src/app/components/new-sandbox/new-sandbox.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import  { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { CommonService } from '../../service/common/common.service';
 import {  INewSandboxObject } from  '../../shared/interface/NewSandboxObject';
 
@@ -18,32 +18,53 @@ export class NewSandboxComponent implements OnInit {
   ownerName: string = "";
   chipBtn1: string = "";
   chipBtn2: string = "";
-  // here we created an Interfeace of the response 
-  //that we are getting from mock-json file
-  
-  newSandboxObject:INewSandboxObject[]= [];
+  newSandboxObject:any = [];
+  sandboxForm : any;
+ 
 
-  constructor(private router: Router, private formBuilder: FormBuilder,
+  constructor(private router: Router,
    private commonService: CommonService ) { }
   
-  sandboxForm = this.formBuilder.group({
-      projectName: ['', Validators.required],
-      description: ['', Validators.required],
-      colorName: ['', Validators.required],
-      tags: [''],
-      prjectLocation: ['']
 
-  });
+
+  // sandboxForm = this.formBuilder.group({
+  //     projectName: ['', Validators.required],
+  //     description: ['', Validators.required],
+  //     colorName: ['', Validators.required],
+  //     tags: [''],
+  //     projectLocation: ['']
+
+  // });
+  
+  createDynamicForm(fields: any) {
+    console.log( fields);
+    let formData:any= {};
+
+    for(let i = 0 ; i < fields.length; i++) {
+      console.log(fields[i].formControlName)
+      console.log(fields[i].isrequired);
+        formData[fields[i].formControlName] =
+        fields[i].isrequired ? 
+        new FormControl('', Validators.required)
+        : new FormControl('');
+    }
+    console.log("formData:");
+    console.log(formData)
+    this.sandboxForm = new FormGroup(formData)
+  }
 
   createSandbox() {
-    this.commonService.setData("projectName", this.sandboxForm.controls['projectName'].value);
-    this.commonService.setData("description", this.sandboxForm.controls['description'].value);
-    this.commonService.setData("colorName", this.sandboxForm.controls['colorName'].value);
+    // this.commonService.setData("projectName", this.sandboxForm.controls['projectName'].value);
+    // this.commonService.setData("description", this.sandboxForm.controls['description'].value);
+    // this.commonService.setData("colorName", this.sandboxForm.controls['colorName'].value);
+    console.log("Inside CreateSandbox() ------------")
+    console.log(this.sandboxForm)
     this.router.navigate(['/createSandbox']);
     }
   back() {
     this.router.navigate(['/sandbox']);
   }
+  
 
   ngOnInit(): void {
       
@@ -56,9 +77,11 @@ export class NewSandboxComponent implements OnInit {
      this.chipBtn2 = this.commonService.getMockData().chipBtn2;
 
      // here we are getting the data from mock-json file
-
      this.newSandboxObject = this.commonService.getMockData().newSandBoxPageDetails;
      console.log(this.newSandboxObject);
+     
+     this.createDynamicForm(this.newSandboxObject.inputFields);
+
   }
 
 }
